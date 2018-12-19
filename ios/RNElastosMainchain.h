a5d932bf1f1b787a231f0995dd812a436478e51b
ademcan <ademcan@ademcan.net>
2018-12-19 08:24:14 +0100

    update bridges to cpp lib

--- ios/RNElastosMainchain.h
+++ ios/RNElastosMainchain.h
@@ -6,5 +6,7 @@
 #import <Foundation/Foundation.h>
 
 @interface RNElastosMainchain : NSObject <RCTBridgeModule>
++ (NSString *)getRootPath;
++ (void)copyFileFromPath:(NSString *)sourcePath toPath:(NSString *)toPath;
 
 @end
