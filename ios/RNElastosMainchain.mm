10a92d2ab4490fcc876482273aec66983b3a746f
ademcan <ademcan@ademcan.net>
2018-12-19 10:57:49 +0100

    Update createWallet bridge

--- ios/RNElastosMainchain.mm
+++ ios/RNElastosMainchain.mm
@@ -63,25 +63,18 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(createWallet: (RCTResponseSenderBlock)callback)
 {
-    
-    //  const std::string rootPath = "/Users/abilican/Documents/projects/perso/elastos/test-demo-rn-app-from-scratch/test/ios/Data";
-    //  const std::string rootPath = "./Data";
     NSString *mRootPath;
     mRootPath = [RNElastosMainchain getRootPath];
     const char *rootPath = [mRootPath UTF8String];
     
     try {
-        
-        
         // MasterWalletManager
         NSLog(@"Creating MasterWallet");
         Elastos::ElaWallet::MasterWalletManager *manager = new Elastos::ElaWallet::MasterWalletManager(rootPath);
         if (manager == nullptr) {
             // fail process
-            NSLog(@"MasterWallet FAILED...");
+            NSLog(@"MasterWalletManager FAILED...");
         }
-        
-        NSLog(@"MasterWallet DONE...");
         try {
             std::vector<Elastos::ElaWallet::IMasterWallet *> masterWallets = manager->GetAllMasterWallets();
             if (masterWallets.size() == 0) {
@@ -94,73 +87,22 @@ RCT_EXPORT_METHOD(createWallet: (RCTResponseSenderBlock)callback)
                     Elastos::ElaWallet::IMasterWallet *masterWallet = manager->CreateMasterWallet(walletID, mnemonic, phrasePassword, payPassword, singleAddress);
                     
                     NSLog(@" MNEMONIC : %s", mnemonic.c_str() );
+                    callback(@[[NSNull null], @(mnemonic.c_str()) ]);
                     
-                } catch (...) {
-                    // exception process
+                } catch (const std::exception &e) {
+                    NSLog(@"Mnemonic Exception : %s", e.what());
                 }
-                
-                
             } else {
                 for (size_t i = 0; i < masterWallets.size(); ++i) {
                     std::vector<Elastos::ElaWallet::ISubWallet *> subWallets = masterWallets[i]->GetAllSubWallets();
-                    // see transaction operation below
                 }
             }
-        } catch (...) {
-            // exception process
-            
+        } catch (const std::exception &e) {
+            NSLog(@"IMasterWallet Exception : %s", e.what());
         }
-        
-        
-        
     } catch (const std::exception &e) {
-        NSLog(@"Exception : %s", e.what());
-        NSLog(@"MasterWallet EXCEPTION...");
-        // exception process
+        NSLog(@"MasterWalletManager Exception : %s", e.what());
     }
-    
-    
-    // TODO: Implement
-    callback(@[[NSNull null], @"Hello from react-native-elastos-mainchain plugin" ]);
 }
-
-
-
-
-//- (dispatch_queue_t)methodQueue
-//{
-//    return dispatch_get_main_queue();
-//}
-//
-//RCT_EXPORT_METHOD(sayHello: (RCTResponseSenderBlock)callback)
-//{
-//    // TODO: Implement
-//    callback(@[[NSNull null], @"Hello from react-native-elastos-mainchain plugin" ]);
-//}
-
-//RCT_EXPORT_METHOD(generateMnemonic: (RCTResponseSenderBlock)callback)
-//{
-//    char* mnemonic = generateMnemonic("english", "");
-//    callback(@[[NSNull null], @(mnemonic) ]);
-//}
-//
-//RCT_EXPORT_METHOD(getSinglePrivateKey: (RCTResponseSenderBlock)callback)
-//{
-//    char* mnemonic = generateMnemonic("english", "");
-//    void* seed;
-//    int seedLen = getSeedFromMnemonic(&seed, mnemonic, "english", "", "");
-//    char* privateKey = getSinglePrivateKey(seed, seedLen);
-//    callback(@[[NSNull null], @(privateKey) ]);
-//}
-//
-//RCT_EXPORT_METHOD(getSinglePublicKey: (RCTResponseSenderBlock)callback)
-//{
-//    char* mnemonic = generateMnemonic("english", "");
-//    void* seed;
-//    int seedLen = getSeedFromMnemonic(&seed, mnemonic, "english", "", "");
-//    char* publicKey = getSinglePublicKey(seed, seedLen);
-//    callback(@[[NSNull null], @(publicKey) ]);
-//}
-
 
 @end
