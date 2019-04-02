b183e3ebd614af8304298f292d3347be7f372a3d
devstar <dev_star11@outlook.com>
2019-04-02 10:44:19 +1000

    update wallet core

--- android/src/main/java/org/elastos/mainchain/reactnative/RNElastosMainchainModule.java
+++ android/src/main/java/org/elastos/mainchain/reactnative/RNElastosMainchainModule.java
@@ -7,7 +7,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-import com.elastos.spvcore.*;
+import org.elastos.elastoswallet.*;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -82,66 +82,66 @@ public class RNElastosMainchainModule extends ReactContextBaseJavaModule {
         callback.invoke(null, newMnemonic);
     }
 
-    @ReactMethod
-    public void importWalletWithMnemonic(String importMnemonic, Callback callback) {
-        masterWallet = walletManager.ImportWalletWithMnemonic(masterWalletId, importMnemonic, phrasePassword, payPassword, singleAddress);
-        String chainID = "ELA";
-        subWallet = masterWallet.CreateSubWallet(chainID, feePerKb);
-        callback.invoke(null, "success");
-    }
-
-    @ReactMethod
-    public void createWallet(String newMnemonic, Callback callback) {
-        masterWallet = walletManager.CreateMasterWallet(masterWalletId, newMnemonic, phrasePassword, payPassword, singleAddress);
-        String chainID = "ELA";
-        subWallet = masterWallet.CreateSubWallet(chainID, feePerKb);
-        callback.invoke(null, "success");
-    }
-
-    @ReactMethod
-    public void getPublicAddress(Callback callback) {
-        String publicAddress = subWallet.CreateAddress();
-        callback.invoke(null, publicAddress);
-    }
-
-    @ReactMethod
-    public void getBalance(Callback callback) {
-        long balance = subWallet.GetBalance();
-        callback.invoke(null, String.valueOf(balance));
-    }
-
-    @ReactMethod
-    public void getTransactionHistory(Callback callback) throws JSONException {
-        int count = 5;
-        JSONArray transactionHistory = new JSONArray();
-        for (int i = 0; count != 0 ; i++){
-            String address = new JSONObject(subWallet.GetAllAddress(i,1)).getJSONArray("Addresses").get(0).toString();
-            try {
-                JSONObject transaction = new JSONObject(subWallet.GetAllTransaction(0, 5, address));
-                if (transaction.isNull("Transactions")) break;
-                JSONArray transactionField = transaction.getJSONArray("Transactions");
-                int length = transactionField.length();
-                if (length == 0) break;
-                for (int j = 0; j < length && count != 0 ; j++) {
-                    JSONObject transactionSummary = transactionField.getJSONObject(j).getJSONObject("Summary");
-                    transactionHistory.put(transactionSummary);
-                    count--;
-                }
-            } catch (Throwable t) {
-                Log.e("My App", "Could not parse malformed JSON");
-            }
-        }
-        callback.invoke(null, transactionHistory.toString());
-    }
-
-    @ReactMethod
-    public void sendToAddress(String amount, String toAddress, Callback callback) {
-        String rawTransaction = subWallet.CreateTransaction("", toAddress, Long.parseLong(amount), "from Mobile Wallet", "remark");
-        long fee = subWallet.CalculateTransactionFee(rawTransaction, feePerKb);
-        rawTransaction = subWallet.UpdateTransactionFee(rawTransaction, fee);
-        rawTransaction = subWallet.SignTransaction(rawTransaction, payPassword);
-        String transactionId = subWallet.PublishTransaction(rawTransaction);
-
-        callback.invoke(null, transactionId);
-    }
+//    @ReactMethod
+//    public void importWalletWithMnemonic(String importMnemonic, Callback callback) {
+//        masterWallet = walletManager.ImportWalletWithMnemonic(masterWalletId, importMnemonic, phrasePassword, payPassword, singleAddress);
+//        String chainID = "ELA";
+//        subWallet = masterWallet.CreateSubWallet(chainID, feePerKb);
+//        callback.invoke(null, "success");
+//    }
+//
+//    @ReactMethod
+//    public void createWallet(String newMnemonic, Callback callback) {
+//        masterWallet = walletManager.CreateMasterWallet(masterWalletId, newMnemonic, phrasePassword, payPassword, singleAddress);
+//        String chainID = "ELA";
+//        subWallet = masterWallet.CreateSubWallet(chainID, feePerKb);
+//        callback.invoke(null, "success");
+//    }
+//
+//    @ReactMethod
+//    public void getPublicAddress(Callback callback) {
+//        String publicAddress = subWallet.CreateAddress();
+//        callback.invoke(null, publicAddress);
+//    }
+//
+//    @ReactMethod
+//    public void getBalance(Callback callback) {
+//        long balance = subWallet.GetBalance();
+//        callback.invoke(null, String.valueOf(balance));
+//    }
+//
+//    @ReactMethod
+//    public void getTransactionHistory(Callback callback) throws JSONException {
+//        int count = 5;
+//        JSONArray transactionHistory = new JSONArray();
+//        for (int i = 0; count != 0 ; i++){
+//            String address = new JSONObject(subWallet.GetAllAddress(i,1)).getJSONArray("Addresses").get(0).toString();
+//            try {
+//                JSONObject transaction = new JSONObject(subWallet.GetAllTransaction(0, 5, address));
+//                if (transaction.isNull("Transactions")) break;
+//                JSONArray transactionField = transaction.getJSONArray("Transactions");
+//                int length = transactionField.length();
+//                if (length == 0) break;
+//                for (int j = 0; j < length && count != 0 ; j++) {
+//                    JSONObject transactionSummary = transactionField.getJSONObject(j).getJSONObject("Summary");
+//                    transactionHistory.put(transactionSummary);
+//                    count--;
+//                }
+//            } catch (Throwable t) {
+//                Log.e("My App", "Could not parse malformed JSON");
+//            }
+//        }
+//        callback.invoke(null, transactionHistory.toString());
+//    }
+//
+//    @ReactMethod
+//    public void sendToAddress(String amount, String toAddress, Callback callback) {
+//        String rawTransaction = subWallet.CreateTransaction("", toAddress, Long.parseLong(amount), "from Mobile Wallet", "remark");
+//        long fee = subWallet.CalculateTransactionFee(rawTransaction, feePerKb);
+//        rawTransaction = subWallet.UpdateTransactionFee(rawTransaction, fee);
+//        rawTransaction = subWallet.SignTransaction(rawTransaction, payPassword);
+//        String transactionId = subWallet.PublishTransaction(rawTransaction);
+//
+//        callback.invoke(null, transactionId);
+//    }
 }
