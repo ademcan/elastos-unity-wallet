1ac1c40240a649ba355a02f236453caa659c5016
devstar <dev_star11@outlook.com>
2019-04-03 22:55:13 +1000

    update core

--- android/src/main/java/org/elastos/mainchain/reactnative/RNElastosMainchainModule.java
+++ android/src/main/java/org/elastos/mainchain/reactnative/RNElastosMainchainModule.java
@@ -7,7 +7,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-import org.elastos.elastoswallet.*;
+import org.elastos.spvcore.*;
 
 import org.json.JSONArray;
 import org.json.JSONException;
