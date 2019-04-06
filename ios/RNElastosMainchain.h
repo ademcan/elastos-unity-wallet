5bc22f60e3a3d617c941a01e1a142732381c2191
ademcan <ademcan@ademcan.net>
2019-04-06 14:55:19 +0200

    Remove submodule
    - create new combined spvsdk library (arm device + simulator)

--- ios/RNElastosMainchain.h
+++ ios/RNElastosMainchain.h
@@ -1,12 +1,8 @@
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#else
-#import "RCTBridgeModule.h"
-#endif
 #import <Foundation/Foundation.h>
 
 @interface RNElastosMainchain : NSObject <RCTBridgeModule>
-+ (NSString *)getRootPath;
-+ (void)copyFileFromPath:(NSString *)sourcePath toPath:(NSString *)toPath;
+//+ (NSString *)getRootPath;
+//+ (void)copyFileFromPath:(NSString *)sourcePath toPath:(NSString *)toPath;
 
 @end
