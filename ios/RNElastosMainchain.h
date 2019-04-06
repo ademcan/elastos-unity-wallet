f89c009d2a1e945fcf6c594724bec2cdcd5ab0a6
ademcan <ademcan@ademcan.net>
2019-04-06 16:54:19 +0200

    Add functions for rootPath

--- ios/RNElastosMainchain.h
+++ ios/RNElastosMainchain.h
@@ -2,7 +2,7 @@
 #import <Foundation/Foundation.h>
 
 @interface RNElastosMainchain : NSObject <RCTBridgeModule>
-//+ (NSString *)getRootPath;
-//+ (void)copyFileFromPath:(NSString *)sourcePath toPath:(NSString *)toPath;
++ (NSString *)getRootPath;
++ (void)copyFileFromPath:(NSString *)sourcePath toPath:(NSString *)toPath;
 
 @end
--- ios/RNElastosMainchain.mm
+++ ios/RNElastosMainchain.mm
@@ -15,6 +15,49 @@
 
 RCT_EXPORT_MODULE()
 
+NSString *mRootPath = [RNElastosMainchain getRootPath];
+const char *rootPath = [mRootPath UTF8String];
+Elastos::ElaWallet::MasterWalletManager *manager = new Elastos::ElaWallet::MasterWalletManager(rootPath);
+
+// Functions required to load the /Data folder properly
++ (NSString *)getRootPath
+{
+    NSString *pathStr = [[NSBundle mainBundle] bundlePath];
+    NSString *path = [NSString stringWithFormat:@"%@/Data", pathStr];
+
+    NSString *toPathStr = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+
+    NSString *toPath = [toPathStr stringByAppendingString:@"/Data"];
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    [fileManager createDirectoryAtPath:toPath withIntermediateDirectories:NO attributes:nil error:nil];
+    [self copyFileFromPath:path toPath:toPath];
+
+    return toPath;
+}
++ (void)copyFileFromPath:(NSString *)sourcePath toPath:(NSString *)toPath
+{
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSError *error = nil;
+    NSArray* array = [fileManager contentsOfDirectoryAtPath:sourcePath error:&error];
+    if(error) {}
+    for(int i = 0; i<[array count]; i++)
+    {
+        NSString *fullPath = [sourcePath stringByAppendingPathComponent:[array objectAtIndex:i]];
+        NSString *fullToPath = [toPath stringByAppendingPathComponent:[array objectAtIndex:i]];
+
+        BOOL isFolder = NO;
+        BOOL isExist = [fileManager fileExistsAtPath:fullPath isDirectory:&isFolder];
+        if (isExist)
+        {
+            NSError *err = nil;[[NSFileManager defaultManager] copyItemAtPath:fullPath toPath:fullToPath error:&err];
+            if (isFolder){
+                [self copyFileFromPath:fullPath toPath:fullToPath];
+            }
+        }
+    }
+}
+
+
 RCT_EXPORT_METHOD(sayHi: (RCTResponseSenderBlock)callback)
 {
     NSLog(@"Hi there...");
